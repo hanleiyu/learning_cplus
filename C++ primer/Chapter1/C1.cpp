686d77f8e7160f76e57c6681c67e326c5a070aa0
hanlei yu <601901861@qq.com>
2025-12-29 12:27:56 +0800

    update chapter1

---
+++ C++ primer/Chapter1/C1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include "Sales_item.h"
+using namespace std;
+
+void main(){
+    // 1.3
+    cout << "Hello World" << endl;
+
+    //1.4
+    cout << 2025 * 12 * 29 << endl;
+    
+    //1.9
+    int sum = 0, val = 50;
+    while(val <= 100){
+        sum += val;
+        ++val;
+    }
+    cout << "sum of 50 to 100 is " << sum << endl;
+
+    //1.11
+    cout << "Please enter two numbers" << endl;
+    int num1, num2;
+    cin >> num1 >> num2;
+    if(num1 > num2){
+        swap(num1, num2);
+    }
+    while(num1 <= num2){
+        cout << num1;
+        num1 ++;
+    }
+    cout << endl;
+}
